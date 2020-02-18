2d78d56e4f18247dd60be35a25ed96a8acdd6598
XMRay <xmr3022@163.com>
2020-02-18 16:15:15 +0800

    真—-计算器窗口版

---
+++ CalculatorWFA/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace CalculatorWFA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b1 = new System.Windows.Forms.Button();
+            this.bd = new System.Windows.Forms.Button();
+            this.bc = new System.Windows.Forms.Button();
+            this.bb = new System.Windows.Forms.Button();
+            this.ba = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.be = new System.Windows.Forms.Button();
+            this.bf = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b0 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.t1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(9, 374);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(75, 23);
+            this.b1.TabIndex = 0;
+            this.b1.TabStop = false;
+            this.b1.Text = "1";
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // bd
+            // 
+            this.bd.Location = new System.Drawing.Point(282, 200);
+            this.bd.Name = "bd";
+            this.bd.Size = new System.Drawing.Size(75, 23);
+            this.bd.TabIndex = 1;
+            this.bd.TabStop = false;
+            this.bd.Text = "/";
+            this.bd.UseVisualStyleBackColor = true;
+            this.bd.Click += new System.EventHandler(this.bd_Click);
+            // 
+            // bc
+            // 
+            this.bc.Location = new System.Drawing.Point(191, 200);
+            this.bc.Name = "bc";
+            this.bc.Size = new System.Drawing.Size(75, 23);
+            this.bc.TabIndex = 2;
+            this.bc.TabStop = false;
+            this.bc.Text = "*";
+            this.bc.UseVisualStyleBackColor = true;
+            this.bc.Click += new System.EventHandler(this.bc_Click);
+            // 
+            // bb
+            // 
+            this.bb.Location = new System.Drawing.Point(100, 200);
+            this.bb.Name = "bb";
+            this.bb.Size = new System.Drawing.Size(75, 23);
+            this.bb.TabIndex = 3;
+            this.bb.TabStop = false;
+            this.bb.Text = "-";
+            this.bb.UseVisualStyleBackColor = true;
+            this.bb.Click += new System.EventHandler(this.bb_Click);
+            // 
+            // ba
+            // 
+            this.ba.Location = new System.Drawing.Point(9, 200);
+            this.ba.Name = "ba";
+            this.ba.Size = new System.Drawing.Size(75, 23);
+            this.ba.TabIndex = 4;
+            this.ba.TabStop = false;
+            this.ba.Text = "+";
+            this.ba.UseVisualStyleBackColor = true;
+            this.ba.Click += new System.EventHandler(this.ba_Click);
+            // 
+            // b7
+            // 
+            this.b7.Location = new System.Drawing.Point(9, 258);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(75, 23);
+            this.b7.TabIndex = 5;
+            this.b7.TabStop = false;
+            this.b7.Text = "7";
+            this.b7.UseVisualStyleBackColor = true;
+            this.b7.Click += new System.EventHandler(this.b7_Click);
+            // 
+            // b8
+            // 
+            this.b8.Location = new System.Drawing.Point(100, 258);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(75, 23);
+            this.b8.TabIndex = 6;
+            this.b8.TabStop = false;
+            this.b8.Text = "8";
+            this.b8.UseVisualStyleBackColor = true;
+            this.b8.Click += new System.EventHandler(this.b8_Click);
+            // 
+            // b9
+            // 
+            this.b9.Location = new System.Drawing.Point(191, 258);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(75, 23);
+            this.b9.TabIndex = 7;
+            this.b9.TabStop = false;
+            this.b9.Text = "9";
+            this.b9.UseVisualStyleBackColor = true;
+            this.b9.Click += new System.EventHandler(this.b9_Click);
+            // 
+            // be
+            // 
+            this.be.Location = new System.Drawing.Point(282, 258);
+            this.be.Name = "be";
+            this.be.Size = new System.Drawing.Size(75, 23);
+            this.be.TabIndex = 8;
+            this.be.TabStop = false;
+            this.be.Text = "=";
+            this.be.UseVisualStyleBackColor = true;
+            this.be.Click += new System.EventHandler(this.be_Click);
+            // 
+            // bf
+            // 
+            this.bf.Location = new System.Drawing.Point(282, 316);
+            this.bf.Name = "bf";
+            this.bf.Size = new System.Drawing.Size(75, 23);
+            this.bf.TabIndex = 9;
+            this.bf.TabStop = false;
+            this.bf.Text = "AC";
+            this.bf.UseVisualStyleBackColor = true;
+            this.bf.Click += new System.EventHandler(this.bf_Click);
+            // 
+            // b6
+            // 
+            this.b6.Location = new System.Drawing.Point(191, 316);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(75, 23);
+            this.b6.TabIndex = 10;
+            this.b6.TabStop = false;
+            this.b6.Text = "6";
+            this.b6.UseVisualStyleBackColor = true;
+            this.b6.Click += new System.EventHandler(this.b6_Click);
+            // 
+            // b4
+            // 
+            this.b4.Location = new System.Drawing.Point(9, 316);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(75, 23);
+            this.b4.TabIndex = 11;
+            this.b4.TabStop = false;
+            this.b4.Text = "4";
+            this.b4.UseVisualStyleBackColor = true;
+            this.b4.Click += new System.EventHandler(this.b4_Click);
+            // 
+            // b5
+            // 
+            this.b5.Location = new System.Drawing.Point(100, 316);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(75, 23);
+            this.b5.TabIndex = 12;
+            this.b5.TabStop = false;
+            this.b5.Text = "5";
+            this.b5.UseVisualStyleBackColor = true;
+            this.b5.Click += new System.EventHandler(this.b5_Click);
+            // 
+            // b0
+            // 
+            this.b0.Location = new System.Drawing.Point(282, 374);
+            this.b0.Name = "b0";
+            this.b0.Size = new System.Drawing.Size(75, 23);
+            this.b0.TabIndex = 13;
+            this.b0.TabStop = false;
+            this.b0.Text = "0";
+            this.b0.UseVisualStyleBackColor = true;
+            this.b0.Click += new System.EventHandler(this.b0_Click);
+            // 
+            // b3
+            // 
+            this.b3.Location = new System.Drawing.Point(191, 374);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(75, 23);
+            this.b3.TabIndex = 14;
+            this.b3.TabStop = false;
+            this.b3.Text = "3";
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.b3_Click);
+            // 
+            // b2
+            // 
+            this.b2.Location = new System.Drawing.Point(100, 374);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(75, 23);
+            this.b2.TabIndex = 15;
+            this.b2.TabStop = false;
+            this.b2.Text = "2";
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.b2_Click);
+            // 
+            // t1
+            // 
+            this.t1.Location = new System.Drawing.Point(25, 42);
+            this.t1.Multiline = true;
+            this.t1.Name = "t1";
+            this.t1.Size = new System.Drawing.Size(312, 107);
+            this.t1.TabIndex = 16;
+            this.t1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 450);
+            this.Controls.Add(this.t1);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b0);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.bf);
+            this.Controls.Add(this.be);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.ba);
+            this.Controls.Add(this.bb);
+            this.Controls.Add(this.bc);
+            this.Controls.Add(this.bd);
+            this.Controls.Add(this.b1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button bd;
+        private System.Windows.Forms.Button bc;
+        private System.Windows.Forms.Button bb;
+        private System.Windows.Forms.Button ba;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button be;
+        private System.Windows.Forms.Button bf;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b0;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.TextBox t1;
+        private System.Windows.Forms.Button b1;
+    }
+}
+
---
+++ CalculatorWFA/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorWFA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double a = 0;
+        double b = 0;
+        double c = 0;
+        int which = 0;
+        bool isN = true;
+        string s;
+        //string sA;
+        //string sB;
+        public void show(int n)
+        {
+            t1.Text = t1.Text + n.ToString();
+           // double d = Convert.ToDouble(n);
+            if (which == 0)
+            {              
+                a = a * 10 + n;
+            }
+            else
+            {
+                b = b * 10 + n;
+            }
+            isN = false;
+        }
+        private void b9_Click(object sender, EventArgs e)
+        {
+            show(9);
+        }
+
+        private void b0_Click(object sender, EventArgs e)
+        {
+            show(0);
+        }
+
+        private void b3_Click(object sender, EventArgs e)
+        {
+            show(3);
+        }
+
+        private void b2_Click(object sender, EventArgs e)
+        {
+            show(2);
+        }
+
+        private void b1_Click(object sender, EventArgs e)
+        {
+            show(1);
+        }
+
+        private void b6_Click(object sender, EventArgs e)
+        {
+            show(6);
+        }
+
+        private void b5_Click(object sender, EventArgs e)
+        {
+            show(5);
+        }
+
+        private void b4_Click(object sender, EventArgs e)
+        {
+            show(4);
+        }
+
+        private void b8_Click(object sender, EventArgs e)
+        {
+            show(8);
+        }
+
+        private void b7_Click(object sender, EventArgs e)
+        {
+            show(7);
+        }
+
+        private void ba_Click(object sender, EventArgs e)
+        {
+            if (isN == false) { 
+                t1.Text = t1.Text + "+";
+                s = "+";
+                which++;
+                isN = true;
+            }    
+        }
+
+        private void bb_Click(object sender, EventArgs e)
+        {
+            if (isN == false)
+            {
+                t1.Text = t1.Text + "-";
+                s = "-";
+                which++;
+                isN = true;
+            }
+        }
+
+        private void bc_Click(object sender, EventArgs e)
+        {
+            if (isN == false)
+            {
+                t1.Text = t1.Text + "*";
+                s = "*";
+                which++;
+                isN = true;
+            }
+        }
+
+        private void bd_Click(object sender, EventArgs e)
+        {
+            if (isN == false)
+            {
+                t1.Text = t1.Text + "/";
+                s = "/";
+                which++;
+                isN = true;
+            }
+        }
+
+        private void be_Click(object sender, EventArgs e)
+        {
+            if (isN == false)
+            {
+                t1.Text = t1.Text + "=";
+                switch (s)
+                {
+                    case "+":
+                        c = a + b;
+                        break;
+                    case "-":
+                        c = a - b;
+                        break;
+                    case "*":
+                        c = a * b;
+                        break;
+                    case "/":
+                        if (b != 0)
+                        {
+                            c = a / b;
+                        }
+                        break;
+                    default:
+                        c = a;
+                        break;
+                }
+                isN = true;
+                string line = "\r\n";
+                t1.Text = t1.Text + c.ToString()+line;
+                a = 0;
+                b = 0;
+                which = 0;
+            }
+        }
+        private void bf_Click(object sender, EventArgs e)
+        {
+            t1.Text = "";
+            c = 0;
+            a = 0;
+            b = 0;
+            which = 0;
+            isN = true;
+
+        }
+    }
+}
+
