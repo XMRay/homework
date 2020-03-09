d4817504555bfb8ed8ed87764b3b5f479648f42b
XMRay <xmr3022@163.com>
2020-03-09 11:47:11 +0800

    第四次课堂作业‘

---
+++ homework4/Clock/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+
+namespace clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock myclock=new Clock();
+            myclock.c1.go(10,36,0);                      //定时
+        }
+    }
+    public delegate void ClockTick(object sender);
+    public delegate void ClockAlarm(object sender);
+
+    public class Clock
+    {
+        public ClockTime c1 = new ClockTime();
+        public Clock()
+        {
+            c1.clockeventA += alarm;
+            c1.clockeventT += tick;
+        }
+        void tick(object sender)
+        {
+            Console.WriteLine($"{c1.hour}:{c1.minute}:{c1.second}");
+        }
+        void alarm(object sender)
+        {
+            Console.WriteLine("时间到了");
+        }
+    }
+    public class ClockTime
+    {
+        public int hour { get; set; }
+        public int minute { get; set; }
+        public int second { get; set; }
+        public event ClockTick clockeventT;
+        public event ClockAlarm clockeventA;
+        
+        public void go(int a,int b,int c)
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+                this.second = DateTime.Now.Second;
+                this.minute = DateTime.Now.Minute;
+                this.hour = DateTime.Now.Hour;
+                clockeventT(this);
+                doalarm(a,b,c);
+            }
+        }
+        public void doalarm(int a, int b, int c)
+        {
+            if (a == this.hour&& b == this.minute&& c == this.second)
+            {
+                clockeventA(this);
+            }
+        }
+    }
+}
---
+++ homework4/Foreach/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApplication
+{
+
+    // 链表节点
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    delegate void Func(string n);
+
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            Node<T> n;
+            n = this.head;
+            while (n != null)
+            {
+                action(n.Data);
+                n = n.Next;
+            }
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GenericList<int> intlist = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+
+            int max = int.MinValue;
+            intlist.ForEach(n => max = (max > n) ? max : n);
+            Console.WriteLine(max);
+            int min = int.MaxValue;
+            intlist.ForEach(n => min = (min < n) ? min : n);
+            Console.WriteLine(min);
+            int total = 0;
+            intlist.ForEach(n => total += n);
+            Console.WriteLine(total);
+
+        }
+
+    }
+}
